b2fccd65fe7619fbd1005ef2e662ed717dc000e5
Anirudha4510 <anirudhamohite023@gmail.com>
2024-07-20 13:36:48 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,46 @@
+let allMusic = [
+    
+    {
+        name: "beautiful Things",
+        artist: "Benson Boon",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Dandelions",
+        artist: "Ruth.B",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Die For You",
+        artist: "weeknd, Ariana Grande",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "We Cant Be Friends",
+        artist: "Ariana Grande",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Story Of My Life",
+        artist: "One Direction",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Summertime Sadness",
+        artist: "Lana Del Rey",
+        img: "music-6",
+        src: "music-6"
+    },
+    {
+        name: "Bling Bang Bang",
+        artist: "Creepy Nuts",
+        img: "music-7",
+        src: "music-7"
+    },
+    
+  ]
